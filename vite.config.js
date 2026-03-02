d5b352935b8ffe1576f522196c7425accbec8fc2
acchewan <acchewan@gmail.com>
2026-03-02 10:52:48 +0545

    Add Firebase Hosting configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "./", // ← relative paths
   plugins: [react()],
-  base: "/React/",
+  build: { outDir: "build" },
 });
